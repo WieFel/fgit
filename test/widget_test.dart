6c9a4c6f5cbc828a969b2928f5656acb8987259c
Felix <felix.wielander@gmail.com>
2021-06-01 17:39:46 +0200

    Renamed app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:fgit/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(FGitApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
