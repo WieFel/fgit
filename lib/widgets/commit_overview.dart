da1ee83538806c0f35dc516e6e18c46fc03a4cc1
Felix <felix.wielander@gmail.com>
2021-06-01 18:25:52 +0200

    Show commit list of current branch

--- lib/widgets/commit_overview.dart
+++ lib/widgets/commit_overview.dart
@@ -8,9 +8,32 @@ class CommitOverview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: Text("Commits..."),
+    return Center(
+      child: FutureBuilder<Map<String, Commit>>(
+        future: gitDir.commits(),
+        builder: (context, snapshot) => snapshot.hasData
+            ? _buildCommitList(snapshot.data)
+            : CircularProgressIndicator(),
+      ),
     );
   }
+
+  Widget _buildCommitList(Map<String, Commit>? commits) => commits != null
+      ? ListView.separated(
+          itemBuilder: (context, index) =>
+              _buildCommit(commits.values.toList()[index]),
+          separatorBuilder: (context, index) => Divider(),
+          itemCount: commits.length)
+      : Center(
+          child: Text("No commits."),
+        );
+
+  Widget _buildCommit(Commit commit) => ListTile(
+        leading: CircleAvatar(child: Text(commit.author[0].toUpperCase())),
+        title: Text(
+          commit.message,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+      );
 }
